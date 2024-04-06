a15f6f445fe9d9b687eee2e325784152a358b359
Tea1me <94841747+Tea1me@users.noreply.github.com>
2024-04-06 20:15:23 +0800

    Add files via upload

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="more-content">直播转换使用方式：
-斗鱼
-  8M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号
-  4M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号/4000
-  2M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号/2000
+斗鱼<br>
+  8M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号<br>
+  4M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号/4000<br>
+  2M：http://xxx.xxx.xxx.xxx:xxxx/douyu/房间号/2000<br>
 
-虎牙
-  4M：http://xxx.xxx.xxx.xxx:xxxx/huya/房间号
-  2M：http://xxx.xxx.xxx.xxx:xxxx/huya/房间号/2000p
+虎牙<br>
+  4M：http://xxx.xxx.xxx.xxx:xxxx/huya/房间号<br>
+  2M：http://xxx.xxx.xxx.xxx:xxxx/huya/房间号/2000p<br>
 
-Bilibili
-  http://xxx.xxx.xxx.xxx:xxxx/bilibili/房间号
+Bilibili<br>
+  http://xxx.xxx.xxx.xxx:xxxx/bilibili/房间号<br>
 </div>
 </template>
 
